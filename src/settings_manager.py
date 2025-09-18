7541257285358553db2caff3c3c51ca0c7cf3017
Vankad24 <vankad24@mail.ru>
2025-09-18 22:21:22 +0800

    Add convert func

--- src/settings_manager.py
+++ src/settings_manager.py
@@ -5,7 +5,7 @@ from src.models.company import CompanyModel
 from src.utils.singletone import Singleton
 
 
-####################################################3
+####################################################
 # Менеджер настроек.
 # Предназначен для управления настройками и хранения параметров приложения
 class SettingsManager(metaclass=Singleton):
@@ -27,14 +27,34 @@ class SettingsManager(metaclass=Singleton):
     # Полный путь к файлу настроек
     @file_name.setter
     def file_name(self, value: str):
-        path = value.strip()
+        path = os.path.abspath(value.strip())
         if path == "":
             return
 
         if os.path.exists(path):
             self.__file_name = path
         else:
-            raise Exception("Не найден файл настроек!")
+            raise Exception(f"Не найден файл настроек: {path}")
+
+    # Конвертация словаря в объект CompanyModel
+    def convert(self, data: dict, to_company_obj=None) -> CompanyModel:
+        if to_company_obj is None:
+            company = CompanyModel()
+        else:
+            company = to_company_obj
+        if "name" in data:
+            company.name = data["name"]
+        if "inn" in data:
+            company.inn = data["inn"]
+        if "account" in data:
+            company.account = data["account"]
+        if "corr_account" in data:
+            company.corr_account = data["corr_account"]
+        if "bik" in data:
+            company.bik = data["bik"]
+        if "ownership" in data:
+            company.ownership = data["ownership"]
+        return company
 
     # Загрузить настройки из Json файла
     def load(self) -> bool:
@@ -44,10 +64,8 @@ class SettingsManager(metaclass=Singleton):
             with open(self.__file_name.strip(), 'r', encoding='utf-8') as file:
                 data = json.load(file)
 
-                if "company" in data.keys():
-                    item = data["company"]
-
-                    self.__company.name = item["name"]
+                if "company" in data:
+                    self.__company = self.convert(data["company"], self.__company)
                     return True
                 return False
         except Exception as e:
