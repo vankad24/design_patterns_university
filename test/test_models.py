e40023e2d5d24c6f0770045804bc887033d515ba
Vankad24 <vankad24@mail.ru>
2025-09-19 15:48:57 +0800

    Add negative tests for model_converter and model_validators

--- test/test_models.py
+++ test/test_models.py
@@ -1,6 +1,6 @@
 import os.path
 
-from src.models.utils import model_converter
+from src.models.utils import model_converter, model_validators
 from src.settings_manager import SettingsManager
 from src.models.company import CompanyModel
 
@@ -77,6 +77,7 @@ class TestModels:
         # Проверки
         assert manager.company == manager2.company
 
+    # Проверка создания CompanyModel из словаря
     def test_convert_creates_company_from_dict(self):
         # Подготовка
         data = {
@@ -99,6 +100,7 @@ class TestModels:
         assert company.bik == data["bik"]
         assert company.ownership == data["ownership"]
 
+    # Проверка создания CompanyModel при загрузке из json файла
     def test_convert_creates_company_from_dir(self):
         # Подготовка
         data = {
@@ -124,6 +126,70 @@ class TestModels:
         assert company.corr_account == data["corr_account"]
         assert company.bik == data["bik"]
         assert company.ownership == data["ownership"]
+
+    # Проверка обработки ошибок при некорректной конвертации CompanyModel
+    # при неправильном параметре data:dict
+    def test_exception_convert_dict_to_company_wrong_dict(self):
+        with pytest.raises(ValueError) as exc_info:
+            model_converter.dict_to_company(None)
+        assert exc_info.value.args[0] == "Для конвертации нужен словарь"
+
+        with pytest.raises(ValueError) as exc_info:
+            model_converter.dict_to_company(["name"])
+        assert exc_info.value.args[0] == "Для конвертации нужен словарь"
+
+    # Проверка обработки ошибок при некорректной конвертации CompanyModel
+    # при отсутствии обязательных ключей в словаре
+    @pytest.mark.parametrize("missing_key", ["name", "inn", "account", "corr_account", "bik", "ownership"])
+    def test_exception_convert_dict_to_company_missing_key(self, missing_key):
+        data = {
+            "name": "ООО Ромашка",
+            "inn": "123456789012",
+            "account": "12345678901",
+            "corr_account": "10987654321",
+            "bik": "123456789",
+            "ownership": "00001"
+        }
+        data.pop(missing_key)
+
+        with pytest.raises(ValueError) as exc_info:
+            model_converter.dict_to_company(data)
+        assert exc_info.value.args[0] == f"В словаре нет обязательного значения '{missing_key}'"
+
+    @pytest.mark.parametrize(
+        "field, value, error_msg",
+        [
+            ("name", "   ", "Наименование не может быть пустым"),
+            ("inn", "123", "ИНН должен содержать ровно 12 символов"),
+            ("inn", "123456789012435", "ИНН должен содержать ровно 12 символов"),
+            ("inn", "12345678901a", "ИНН должен содержать только цифры"),
+            ("account", "123", "Счет должен содержать ровно 11 символов"),
+            ("account", "1234567890a", "Счет должен содержать только цифры"),
+            ("corr_account", "123", "Корреспондентский счет должен содержать ровно 11 символов"),
+            ("corr_account", "1234567890a", "Корреспондентский счет должен содержать только цифры"),
+            ("bik", "123", "БИК должен содержать ровно 9 символов"),
+            ("bik", "12345678a", "БИК должен содержать только цифры"),
+            ("ownership", "1234", "Вид собственности должен содержать ровно 5 символов"),
+            ("ownership", "abda", "Вид собственности должен содержать ровно 5 символов"),
+            ("ownership", "1234a234ds", "Вид собственности должен содержать ровно 5 символов"),
+        ]
+    )
+    def test_validate_company_fields(self, field, value, error_msg):
+        data = {
+            "name": "ООО Ромашка",
+            "inn": "123456789012",
+            "account": "12345678901",
+            "corr_account": "10987654321",
+            "bik": "123456789",
+            "ownership": "00001"
+        }
+
+        data[field] = value
+        company = model_converter.dict_to_company(data)
+
+        with pytest.raises(ValueError) as exc_info:
+            model_validators.validate_company(company)
+        assert exc_info.value.args[0] == error_msg
 
 
 if __name__ == "__main__":
