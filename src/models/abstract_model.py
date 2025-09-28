06b724ef7fb536c69431d34e4094e37bd70b89bb
Vankad24 <vankad24@mail.ru>
2025-09-28 15:54:35 +0800

    Change load_from_dict func

--- src/models/abstract_model.py
+++ src/models/abstract_model.py
@@ -4,11 +4,10 @@ from abc import abstractmethod, ABC
 class AbstractModel(ABC):
     __id: str = uuid.uuid4().hex
 
-    def load_from_dict(self, data:dict):
-        public_fields = set(filter(lambda x: not x.startswith("_"), dir(self)))
-        matching_keys = public_fields & data.keys()
-        for key in matching_keys:
-            setattr(self, key, data[key])
+    def load_from_dict(self, data: dict):
+        for key, value in data.items():
+            if not key.startswith("_") and hasattr(self, key):
+                setattr(self, key, value)
 
     @property
     def id(self) -> str:
