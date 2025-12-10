bdecd64a0c754730bcf6472250bb81924bd64b40
Vankad24 <vankad24@mail.ru>
2025-12-10 22:09:01 +0800

    Update observer

--- src/core/observer/event_type.py
+++ src/core/observer/event_type.py
@@ -2,21 +2,9 @@ from enum import StrEnum
 
 
 class EventType(StrEnum):
+    BROADCAST = "BROADCAST"
     MODEL_ADD = "MODEL_ADD"
-    MODEL_REMOVE = "MODEL_REMOVE"
-
-    @classmethod
-    def values(cls) -> list[str]:
-        return list(map(str, cls))
-
-    @classmethod
-    def get_names(cls) -> list[str]:
-        return list(map(lambda x: x.name, cls))
-
-    @classmethod
-    def has(cls, val):
-        if isinstance(val, cls):
-            return val in cls
-        return val in cls.get_names()
-
-    # def __eq__(self, other):
+    MODEL_DELETE = "MODEL_DELETE"
+    MODEL_CHANGE = "MODEL_CHANGE"
+    MODEL_OPERATION_ERROR = "MODEL_OPERATION_ERROR"
+    MODEL_OPERATION_SUCCESS = "MODEL_OPERATION_SUCCESS"
--- src/core/observer/observer_service.py
+++ src/core/observer/observer_service.py
@@ -1,6 +1,6 @@
+from typing import Callable
 
-from src.core.observer.event_listener import EventListener
-from src.core.observer.event_params import AbstractEventParams
+from src.core.observer.event_models import AbstractEvent
 from src.core.observer.event_type import EventType
 from src.core.singletone import Singleton
 from src.models.validators.functions import validate_val
@@ -11,31 +11,36 @@ from src.models.validators.functions import validate_val
 class ObserverService(metaclass=Singleton):
 
     def __init__(self):
-        self.listeners: dict[EventType, list[EventListener]] = {k: list() for k in EventType.values()}
+        self.callbacks: dict[EventType, list[Callable]] = {str(k): list() for k in EventType}
 
     """
-    Добавить слушателя
     """
-    def add(self, event: str|EventType, listener: EventListener):
-        validate_val(listener, EventListener)
-        self.listeners[EventType(event)].append(listener)
+    def add(self, event_type: str | EventType, callback: Callable):
+        validate_val(callback, Callable)
+        self.callbacks[EventType(event_type)].append(callback)
 
     """
-    Удалить слушателя
     """
-    def delete(self, event: str|EventType,  listener: EventListener) -> bool:
-        listeners = self.listeners[EventType(event)]
-        if listener in listeners:
-            listeners.remove(listener)
+    def delete(self, event_type: str | EventType, callback: Callable) -> bool:
+        listeners = self.callbacks[EventType(event_type)]
+        if callback in listeners:
+            listeners.remove(callback)
             return True
         return False
 
     """
-    Вызвать событие
     """
-    def make_event(self, event: str|EventType, params: AbstractEventParams):
-        for listener in self.listeners[EventType(event)]:
-            need_to_stop = listener.handle(event, params)
+    def make_event(self, event: AbstractEvent):
+        validate_val(event, AbstractEvent)
+        for callback in self.callbacks[event.event_type]:
+            need_to_stop = callback(event)
+            # Если вернулось True, останавливаем обработку события
+            if need_to_stop:
+                break
+
+        # Широковещательный
+        for callback in self.callbacks[EventType.BROADCAST]:
+            need_to_stop = callback(event)
             # Если вернулось True, останавливаем обработку события
             if need_to_stop:
                 break
